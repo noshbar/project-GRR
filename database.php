0bfc3f56e2f4617a72e39eeaa797ace5021bc9f1
NoshBar <noshbar@hotmail.com>
2013-04-09 21:14:59 +0200

    Added the basics for adding and removing tags from items. This will be used to mark favourites, create categories, whatever. TODO: send changes to database.

--- database.php
+++ database.php
@@ -15,9 +15,11 @@ function openDatabase($Filename)
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		if (!$existed)
 		{
-			$tables['site']     = 'CREATE TABLE site(id INTEGER PRIMARY KEY, name TEXT UNIQUE, source TEXT, deleted BOOLEAN DEFAULT 0);';
-			$tables['contents'] = 'CREATE VIRTUAL TABLE contents USING fts4(title, body);';
-			$tables['item']     = 'CREATE TABLE item(id INTEGER PRIMARY KEY, guid TEXT UNIQUE, source TEXT, deleted BOOLEAN DEFAULT 0, siteId INTEGER, timestamp DATETIME, contentId INTEGER NOT NULL, read BOOLEAN DEFAULT 0);';
+			$tables['tags']        = 'CREATE TABLE tags(id INTEGER PRIMARY KEY, name TEXT UNIQUE);';
+			$tables['taggedItems'] = 'CREATE TABLE taggedItems(id INTEGER PRIMARY KEY, tagId INTEGER, itemId INTEGER);';
+			$tables['site']        = 'CREATE TABLE site(id INTEGER PRIMARY KEY, name TEXT UNIQUE, source TEXT, deleted BOOLEAN DEFAULT 0);';
+			$tables['contents']    = 'CREATE VIRTUAL TABLE contents USING fts4(title, body);';
+			$tables['item']        = 'CREATE TABLE item(id INTEGER PRIMARY KEY, guid TEXT UNIQUE, source TEXT, deleted BOOLEAN DEFAULT 0, siteId INTEGER, timestamp DATETIME, contentId INTEGER NOT NULL, read BOOLEAN DEFAULT 0);';
 			foreach ($tables as $table)
 			{	
 				$result = $db->exec($table);
@@ -26,6 +28,7 @@ function openDatabase($Filename)
 					quit('Could not create table: '.$table."\n");
 				}
 			}
+			$db->exec("INSERT INTO tags(name) VALUES('Favourites')");
 		}
 		return $db;	
 	}	
